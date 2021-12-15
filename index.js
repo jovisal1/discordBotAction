d0744798ebc2692934d61a567e7b2336b72e2fc9
jovisal1 <pvidalsalvador@gmail.com>
2021-12-15 12:41:40 +0100

    Action created

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const core = require("@actions/core");
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+const discord_token = core.getInput("discord_token");
+const id_channel = core.getInput("id_channel");
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user}!`);
+  client.channels
+    .fetch(id_channel)
+    .then((channel) => {
+      channel.send(`Vamos que nos vamos desde nodeaaaa`);
+    })
+    .catch((err) => console.log("Could not find the channel."));
+});
+
+client.login(discord_token);
